2689a58019367b3cac3bfd3058bff81b6c2de676
hstanev <hstanev@gmail.com>
2015-11-25 21:40:32 +0200

    Use django.utils.timezone instead of datetime.timezone - now works with python2 too

--- locking/models.py
+++ locking/models.py
@@ -1,7 +1,8 @@
 import logging
-from datetime import datetime, timezone
+from datetime import datetime
 
 from django.db import models
+from django.utils import timezone
 
 # Forward compat with Django 1.5's custom user models
 from django.conf import settings
@@ -145,7 +146,7 @@ class Lock(models.Model):
         """
         if not isinstance(self.locked_at, datetime):
             return False
-        return datetime.now(timezone.utc) < self.lock_expiration_time
+        return timezone.now() < self.lock_expiration_time
 
 
     @property
@@ -172,7 +173,7 @@ class Lock(models.Model):
         """
         if not self.locked_at:
             return 0
-        locked_delta = datetime.now(timezone.utc) - self.locked_at
+        locked_delta = timezone.now() - self.locked_at
         # If the lock has already expired, there are 0 seconds remaining
         if locking_settings.TIME_UNTIL_EXPIRATION < locked_delta:
             return 0
@@ -211,7 +212,7 @@ class Lock(models.Model):
             else:
                 raise ObjectLockedError("This object is already locked by another"
                     " user. May not override, except through the `unlock` method.")
-        locked_at = datetime.now(timezone.utc)
+        locked_at = timezone.now()
         if lock_duration:
             locked_at += lock_duration - locking_settings.TIME_UNTIL_EXPIRATION
         self._locked_at = locked_at
